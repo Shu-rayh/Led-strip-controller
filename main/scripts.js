4275cb68cc7a42a6b7aca4e7dd46fac34febb080
Shurayh Sheik <shurayho1@gmail.com>
2026-09-07 01:17:56 +0200

    added code & javascript  for on/off, color slider and brightnessor

--- main/scripts.js
+++ main/scripts.js
@@ -15,4 +15,58 @@ async function connect() {
     
 }
 
-document.getElementById("connectButton").addEventListener("click", function() {})
+document.getElementById("connectButton").addEventListener("click", function() {});
+
+async function turnOn() {
+    let bytes = new Uint8Array([0x7e, 0xff, 0x04, 0x01, 0xff, 0xff, 0xff, 0xff, 0xef]);
+    await characteristic.writeValueWithResponse(bytes);
+
+}
+
+async function turnOff() {
+    let bytes = new Uint8Array([0x7e, 0xff, 0x04, 0x00, 0xff, 0xff, 0xff, 0xff, 0xef]);
+    await characteristic.writeValueWithResponse(bytes);
+    
+}
+
+document.getElementById("offButton").addEventListener("click", function() {
+    turnOff();
+})
+
+function syncPair(sliderId, numberId) {
+    let slider = document.getElementById(sliderId);
+    let number = document.getElementById(numberId);
+    slider.addEventListener("input", function() {
+        number.value = slider.value;
+    });
+    
+}
+
+syncPair("sliderR", "numberR");
+syncPair("sliderG", "numberG");
+syncPair("sliderB", "numberB");
+
+async function  setColor(r, g, b) {
+    let bytes = new Uint8Array([0x7e, 0xff, 0x05, 0x03, r, g, b, 0xff, 0xef])
+    await characteristic.writeValueWithResponse(bytes);
+}
+documen.getElementById("sendColorButton").addEventListener("click", function() {
+    let r = parseInt (document.getElementById("numberR").value);
+    let g = parseInt (document.getElementById("numberG").value);
+    let b = parseInt (document.getElementById("numberB").value);
+    setColor(r, g, b);
+})
+
+async function setBrightness(percent) {
+    let bytes = new Uint8Array([0x7e, 0xff, 0x01, percent, 0x00, 0xff,0xff, 0xef]);
+    await characteristic.writeValueWithResponse(bytes);
+    
+}
+
+document.getElementById("sliderBright").addEventListener("input", function() {
+    let percent = parseInt(this.value);
+    document.getElementById("brightLabel").textContent = percent + "%";
+    setBrightness(percent);
+
+
+})
